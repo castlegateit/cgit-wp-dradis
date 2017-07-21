6aba6c47e428e624a508045f877c8c8ed3d1c909
John Hughes <john@castlegateit.co.uk>
2017-07-21 09:32:14 +0100

    Refactor

--- cgit-wp-dradis.php
+++ cgit-wp-dradis.php
@@ -5,25 +5,30 @@
 Plugin Name: Castlegate IT WP Dradis
 Plugin URI: http://github.com/castlegateit/cgit-wp-dradis
 Description: Detects unwanted robots.
-Version: 1.0
+Version: 1.1
 Author: Castlegate IT
 Author URI: http://www.castlegateit.co.uk/
 License: MIT
 
 */
 
-if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/robots.txt')) {
-    add_action('admin_notices', function() {
-        echo '<div class="error"><p><strong>Warning:</strong>'
-            . ' <code>robots.txt</code> detected.</p></div>';
+$warn = function ($content) {
+    add_action('admin_notices', function () use ($content) {
+        ?>
+        <div class="error">
+            <p><strong>Warning:</strong> <?= $content ?></p>
+        </div>
+        <?php
     });
+};
+
+if (file_exists(ABSPATH . 'robots.txt')) {
+    $warn('<code>robots.txt</code> detected.');
 }
 
 if (get_option('blog_public') == 0) {
-    add_action('admin_notices', function() {
-        echo '<div class="error"><p><strong>Warning:</strong>'
-            . ' search engines are being blocked by <a href="'
-            . admin_url() . 'options-reading.php">WordPress settings</a>'
-            . '.</p></div>';
-    });
+    $warn('search engines blocked by <a href="' . admin_url()
+        . 'options-reading.php">WordPress settings</a>.');
 }
+
+do_action('cgit_dradis_loaded');
